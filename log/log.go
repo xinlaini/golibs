025bd309eebe9d6f292877c146b4c60a51a609b0
Xinlai Ni <xinlaini@gmail.com>
2016-01-21 13:25:58 -0800

    log again

--- log/log.go
+++ log/log.go
@@ -1,4 +1,4 @@
-package log
+package xlog
 
 import (
 	"flag"
@@ -19,7 +19,6 @@ const (
 
 var (
 	DefaultLogger = NewLogger(os.Stdout, os.Stderr)
-	console       = newConsoleLogger()
 	colorConsole  = flag.Bool("color_console", true, "Whether to print colored console log")
 )
 
@@ -36,38 +35,40 @@ type Logger interface {
 }
 
 type impl struct {
-	linfo  *log.Logger
-	lerr   *log.Logger
-	lfatal *log.Logger
-	lpanic *log.Logger
+	linfo   *log.Logger
+	lerr    *log.Logger
+	lfatal  *log.Logger
+	lpanic  *log.Logger
+	colored bool
 }
 
 // NewLogger creates new logger using stdout and stderr files.
 func NewLogger(stdout, stderr *os.File) Logger {
 	return &impl{
-		linfo:  log.New(stdout, infoSign, flags),
-		lerr:   log.New(stderr, errorSign, flags),
-		lfatal: log.New(stderr, fatalSign, flags),
-		lpanic: log.New(stderr, panicSign, flags),
+		linfo:  log.New(stdout, "[INFO ", flags),
+		lerr:   log.New(stderr, "[ERRO ", flags),
+		lfatal: log.New(stderr, "[FATA ", flags),
+		lpanic: log.New(stderr, "[PANI ", flags),
 	}
 }
 
-func newConsoleLogger() *impl {
+func NewConsoleLogger() Logger {
 	if !*colorConsole {
-		return NewLogger(os.Stdout, os.Stderr).(*impl)
+		return NewLogger(os.Stdout, os.Stderr)
 	}
 
 	return &impl{
-		linfo:  log.New(os.Stdout, "\x1b[1;32m"+infoSign, flags),
-		lerr:   log.New(os.Stderr, "\x1b[1;31m"+errorSign, flags),
-		lfatal: log.New(os.Stderr, "\x1b[1;35m"+fatalSign, flags),
-		lpanic: log.New(os.Stderr, "\x1b[1;33m"+panicSign, flags),
+		linfo:   log.New(os.Stdout, "\x1b[1;32m"+infoSign, flags),
+		lerr:    log.New(os.Stderr, "\x1b[1;31m"+errorSign, flags),
+		lfatal:  log.New(os.Stderr, "\x1b[41;30m"+fatalSign, flags),
+		lpanic:  log.New(os.Stderr, "\x1b[43;30m"+panicSign, flags),
+		colored: true,
 	}
 }
 
-func suffix(resetColor bool) string {
+func (l *impl) suffix() string {
 	if _, file, line, ok := runtime.Caller(2); ok {
-		if resetColor {
+		if l.colored {
 			return fmt.Sprintf("%s:%d]\x1b[0m", path.Base(file), line)
 		}
 		return fmt.Sprintf("%s:%d]", path.Base(file), line)
@@ -77,84 +78,42 @@ func suffix(resetColor bool) string {
 
 // Info log.
 func (l *impl) Info(v ...interface{}) {
-	l.linfo.Println(suffix(false), fmt.Sprint(v...))
+	l.linfo.Println(l.suffix(), fmt.Sprint(v...))
 }
 
 // Infof log.
 func (l *impl) Infof(format string, v ...interface{}) {
-	l.linfo.Println(suffix(false), fmt.Sprintf(format, v...))
+	l.linfo.Println(l.suffix(), fmt.Sprintf(format, v...))
 }
 
 // Error log.
 func (l *impl) Error(v ...interface{}) {
-	l.lerr.Println(suffix(false), fmt.Sprint(v...))
+	l.lerr.Println(l.suffix(), fmt.Sprint(v...))
 }
 
 // Errorf log.
 func (l *impl) Errorf(format string, v ...interface{}) {
-	l.lerr.Println(suffix(false), fmt.Sprintf(format, v...))
+	l.lerr.Println(l.suffix(), fmt.Sprintf(format, v...))
 }
 
 // Fatal log.
 func (l *impl) Fatal(v ...interface{}) {
-	l.lfatal.Println(suffix(false), fmt.Sprint(v...))
+	l.lfatal.Println(l.suffix(), fmt.Sprint(v...))
 }
 
 // Fatalf log.
 func (l *impl) Fatalf(format string, v ...interface{}) {
-	l.lfatal.Println(suffix(false), fmt.Sprintf(format, v...))
+	l.lfatal.Println(l.suffix(), fmt.Sprintf(format, v...))
 }
 
 // Panic log.
 func (l *impl) Panic(v ...interface{}) {
-	l.lpanic.Println(suffix(false), fmt.Sprint(v...))
+	l.lpanic.Println(l.suffix(), fmt.Sprint(v...))
 	panic("")
 }
 
 // Panicf log.
 func (l *impl) Panicf(format string, v ...interface{}) {
-	l.lpanic.Println(suffix(false), fmt.Sprintf(format, v...))
-	panic("")
-}
-
-// Info log.
-func Info(v ...interface{}) {
-	console.linfo.Println(suffix(*colorConsole), fmt.Sprint(v...))
-}
-
-// Infof log.
-func Infof(format string, v ...interface{}) {
-	console.linfo.Println(suffix(*colorConsole), fmt.Sprintf(format, v...))
-}
-
-// Error log.
-func Error(v ...interface{}) {
-	console.lerr.Println(suffix(*colorConsole), fmt.Sprint(v...))
-}
-
-// Errorf log.
-func Errorf(format string, v ...interface{}) {
-	console.lerr.Println(suffix(*colorConsole), fmt.Sprintf(format, v...))
-}
-
-// Fatal log.
-func Fatal(v ...interface{}) {
-	console.lfatal.Fatalln(suffix(*colorConsole), fmt.Sprint(v...))
-}
-
-// Fatalf log.
-func Fatalf(format string, v ...interface{}) {
-	console.lfatal.Fatalln(suffix(*colorConsole), fmt.Sprintf(format, v...))
-}
-
-// Panic log.
-func Panic(v ...interface{}) {
-	console.lpanic.Println(suffix(*colorConsole), fmt.Sprint(v...))
-	panic("")
-}
-
-// Panicf log.
-func Panicf(format string, v ...interface{}) {
-	console.lpanic.Println(suffix(*colorConsole), fmt.Sprintf(format, v...))
+	l.lpanic.Println(l.suffix(), fmt.Sprintf(format, v...))
 	panic("")
 }
