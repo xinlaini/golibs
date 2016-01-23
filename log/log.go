174288089e2ab6decd5bea2dd70fef763436a346
Xinlai Ni <xinlaini@gmail.com>
2016-01-23 08:42:56 -0800

    nil and plain logger

--- log/log.go
+++ log/log.go
@@ -3,6 +3,7 @@ package xlog
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
@@ -29,7 +30,7 @@ type Logger interface {
 	Panicf(format string, v ...interface{})
 }
 
-type impl struct {
+type pretty struct {
 	linfo   *log.Logger
 	lerr    *log.Logger
 	lfatal  *log.Logger
@@ -39,7 +40,7 @@ type impl struct {
 
 // NewLogger creates new logger using stdout and stderr files.
 func NewLogger(stdout, stderr *os.File) Logger {
-	return &impl{
+	return &pretty{
 		linfo:  log.New(stdout, "[INFO ", flags),
 		lerr:   log.New(stderr, "[ERRO ", flags),
 		lfatal: log.New(stderr, "[FATA ", flags),
@@ -52,7 +53,7 @@ func NewConsoleLogger() Logger {
 		return NewLogger(os.Stdout, os.Stderr)
 	}
 
-	return &impl{
+	return &pretty{
 		linfo:   log.New(os.Stdout, "\x1b[1;32m[", flags),
 		lerr:    log.New(os.Stderr, "\x1b[1;31m[", flags),
 		lfatal:  log.New(os.Stderr, "\x1b[1;35m[", flags),
@@ -61,7 +62,7 @@ func NewConsoleLogger() Logger {
 	}
 }
 
-func (l *impl) suffix() string {
+func (l *pretty) suffix() string {
 	if _, file, line, ok := runtime.Caller(2); ok {
 		if l.colored {
 			return fmt.Sprintf("%s:%d]\x1b[0m", path.Base(file), line)
@@ -72,43 +73,104 @@ func (l *impl) suffix() string {
 }
 
 // Info log.
-func (l *impl) Info(v ...interface{}) {
+func (l *pretty) Info(v ...interface{}) {
 	l.linfo.Println(l.suffix(), fmt.Sprint(v...))
 }
 
 // Infof log.
-func (l *impl) Infof(format string, v ...interface{}) {
+func (l *pretty) Infof(format string, v ...interface{}) {
 	l.linfo.Println(l.suffix(), fmt.Sprintf(format, v...))
 }
 
 // Error log.
-func (l *impl) Error(v ...interface{}) {
+func (l *pretty) Error(v ...interface{}) {
 	l.lerr.Println(l.suffix(), fmt.Sprint(v...))
 }
 
 // Errorf log.
-func (l *impl) Errorf(format string, v ...interface{}) {
+func (l *pretty) Errorf(format string, v ...interface{}) {
 	l.lerr.Println(l.suffix(), fmt.Sprintf(format, v...))
 }
 
 // Fatal log.
-func (l *impl) Fatal(v ...interface{}) {
+func (l *pretty) Fatal(v ...interface{}) {
 	l.lfatal.Fatalln(l.suffix(), fmt.Sprint(v...))
 }
 
 // Fatalf log.
-func (l *impl) Fatalf(format string, v ...interface{}) {
+func (l *pretty) Fatalf(format string, v ...interface{}) {
 	l.lfatal.Fatalln(l.suffix(), fmt.Sprintf(format, v...))
 }
 
 // Panic log.
-func (l *impl) Panic(v ...interface{}) {
+func (l *pretty) Panic(v ...interface{}) {
 	l.lpanic.Println(l.suffix(), fmt.Sprint(v...))
 	panic("")
 }
 
 // Panicf log.
-func (l *impl) Panicf(format string, v ...interface{}) {
+func (l *pretty) Panicf(format string, v ...interface{}) {
 	l.lpanic.Println(l.suffix(), fmt.Sprintf(format, v...))
+	panic("")
+}
+
+type plain struct {
+	lout *log.Logger
+	lerr *log.Logger
+}
+
+func NewPlainLogger() Logger {
+	return &plain{
+		lout: log.New(os.Stdout, "", 0),
+		lerr: log.New(os.Stderr, "", 0),
+	}
+}
+
+func NewNilLogger() Logger {
+	return &plain{
+		lout: log.New(ioutil.Discard, "", 0),
+		lerr: log.New(ioutil.Discard, "", 0),
+	}
+}
+
+// Info log.
+func (pl *plain) Info(v ...interface{}) {
+	pl.lout.Println(v...)
+}
+
+// Infof log.
+func (pl *plain) Infof(format string, v ...interface{}) {
+	pl.lout.Println(fmt.Sprintf(format, v...))
+}
+
+// Error log.
+func (pl *plain) Error(v ...interface{}) {
+	pl.lerr.Println(v...)
+}
+
+// Errorf log.
+func (pl *plain) Errorf(format string, v ...interface{}) {
+	pl.lerr.Println(fmt.Sprintf(format, v...))
+}
+
+// Fatal log.
+func (pl *plain) Fatal(v ...interface{}) {
+	pl.lerr.Fatalln(v...)
+}
+
+// Fatalf log.
+func (pl *plain) Fatalf(format string, v ...interface{}) {
+	pl.lerr.Fatalln(fmt.Sprintf(format, v...))
+}
+
+// Panic log.
+func (pl *plain) Panic(v ...interface{}) {
+	pl.lerr.Println(v...)
+	panic("")
+}
+
+// Panicf log.
+func (pl *plain) Panicf(format string, v ...interface{}) {
+	pl.lerr.Println(fmt.Sprintf(format, v...))
 	panic("")
 }
